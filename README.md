c45b8386f6957a276dbd0c4f6e0e0c23af7126df
hzhanhao <hzhanhao@corp.netease.com>
2014-12-16 17:34:04 +0800

    update pwdfilepasswd.sh
    Change-Id: I5d6a0427a4a932db96249f104f70acc6a65a5dc3

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # vsftpd-shell
 
-**vsftpd**使用了**pam**认证：Ladp、Berkeley db、libpam-pwdfile.
+**vsftpd**使用了**pam**认证：Ldap、Berkeley db、libpam-pwdfile.
 
 * check-user.sh 检查一个账号是否已经存在。
-* pwdfilepasswd.sh pwdfile 认证账号加密密码设置。
+* pwdfilepasswd.sh 生成 pwdfile 认证账号记录。
 * del-DB-user.py 删除 Berkeley db 中的 ftp 认证账号。
 * add-or-update-DB-user.py 增加 Berkeley db 中的 ftp 认证账号，或为已存在的账号更新密码。
--- pwdfilepasswd.sh
+++ pwdfilepasswd.sh
@@ -1,1 +1,11 @@
-perl -e '$salt=q($1$).int(rand(1e8)); print "password: "; chomp($passwd=<STDIN>); print crypt($passwd,$salt),"\n"'
+#!/bin/sh
+
+#################################
+#  add pwdfile ftp user record  #
+#################################
+
+export user=$1
+export passwd=$2
+
+#perl -e '$salt=q($1$).int(rand(1e8)); print "password: "; chomp($passwd=<STDIN>); print crypt($passwd,$salt),"\n"'
+perl -e '$salt=q($1$).int(rand(1e8)); print $ENV{"user"},":",crypt($ENV{"passwd"},$salt),"\n"'
